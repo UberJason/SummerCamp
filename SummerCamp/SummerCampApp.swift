52f9bf4ffd1c9e97e2a24f05b5cc783bd75f8770
Jason Ji <uberjason@gmail.com>
2024-06-08 11:52:02 -0400

    Initial Commit

---
+++ SummerCamp/SummerCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerCampApp.swift
+//  SummerCamp
+//
+//  Created by Jason Ji on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SummerCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
